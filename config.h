cfa768b2aa40ea4efda32f467f9f0172833e8c0e
rafa_99 <rafa99@protonmail.com>
2019-10-06 12:20:39 +0000

    Centered Master Patch

--- config.h
+++ config.h
@@ -48,6 +48,8 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 	{ NULL,       NULL },
 };
 
