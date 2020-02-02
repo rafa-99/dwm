afc5f58422f6f6d62872b52b59035c4306f64f0a
rafa_99 <rafa99@protonmail.com>
2020-02-02 12:55:49 +0000

    Updated DWM

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
@@ -29,7 +29,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "爵", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "爵", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
