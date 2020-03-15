12f12252806d8d3cf7e7147eb3de5247147a834e
rafa_99 <rafa99@protonmail.com>
2020-03-15 13:33:19 +0000

    Updated Font and WM Tags

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "UbuntuMono Nerd Font:size=11" };
+static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=10" };
 #include "colors.h"
 static const char *colors[][3]      = {
 	/*               fg           bg         border   */
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "爵", "", "", "", "", "", "", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
