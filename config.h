1fb0f07ce5fbc026049fb178989ef22d24eaa762
rafa_99 <rafa99@protonmail.com>
2020-09-02 19:58:59 +0100

    Updated dmenucmd

--- config.h
+++ config.h
@@ -73,7 +73,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run","-i", NULL };
+static const char *dmenucmd[] = { "dmenu_run","-i", "-c", "-l", "15", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
