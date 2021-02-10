a1a7f98e26f914c94f1139ac9176bf3481951556
rafa_99 <rafa99@protonmail.com>
2021-02-10 18:29:26 +0000

    Revert "Updated Stock DWM Layout"
    This reverts commit 74b85500d56db7abdd3f0e6574d25165dd122812.

--- config.h
+++ config.h
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.95; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
@@ -45,9 +45,9 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ ">M>",      centeredfloatingmaster },    /* first entry is default */
+	{ "[]=",      tile },    /* first entry is default */
+	{ ">M>",      centeredfloatingmaster },
 	{ "|M|",      centeredmaster },
-	{ "[]=",      tile },
 	{ "[@]",      spiral },
 	{ "[\\]",     dwindle },
 	{ "H[]",      deck },
