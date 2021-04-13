6e4110994913ea19e55b80937b1bedbfb139748e
rafa_99 <rafa99@protonmail.com>
2021-04-13 13:54:34 +0100

    Updated Border Size

--- config.h
+++ config.h
@@ -3,13 +3,13 @@
 #include "colors.h"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "FiraCode Nerd Font:size=12" };
+static const char *fonts[]          = { "FiraCode Nerd Font:size=14" };
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { norm_fg, norm_bg, norm_border }, // Unfocused Windows
