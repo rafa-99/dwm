bff0cf67d26ff4639a72961f606a011f81ced427
rafa_99 <raroma09@gmail.com>
2022-06-26 23:06:53 +0100

    Status padding patch

--- config.def.h
+++ config.def.h
@@ -13,6 +13,8 @@ static int smartgaps                = 0;        /* 1 means no outer gap when the
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
